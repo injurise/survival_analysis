afe17955c6189387f0af2fd2939ea55dc44e2092
pnd-amoellers <Alexander.j.moellers@gmail.com>
2022-04-25 23:34:11 +0200

    updated grid search file

--- run_pipeline/grid_search_cpath_mdrop_lingau_bnn.py
+++ run_pipeline/grid_search_cpath_mdrop_lingau_bnn.py
@@ -4,7 +4,7 @@ def sub_command(cmd,identifier, hours=4):
     setup_cmd = 'conda activate /cluster/home/amollers/software/anaconda/envs/base_env'
     # TODO: adjust this to needs with -n {cpus} and mem as param
     sub_cmd = (f'bsub -W {hours}:00 -n 4 -R "rusage[mem=2000]" '
-               f'-o logs/{identifier}.log -J {identifier} "{setup_cmd}; {cmd}"')
+               f'-o logs/{identifier}.log -J {identifier} -eo err_logs/{identifier}.err "{setup_cmd}; {cmd}"')
     os.system(sub_cmd)
 
 learning_rates = [0.05,0.6]
@@ -19,9 +19,9 @@ for learning_rate in learning_rates:
             for num_mc in num_mcs:
 
                 cmd =(f'cpath_mdrop_lingau_bnn_bsub.py --epochs {epoch} ' 
-                      f'--num-mc {num_mc} --lr {learning_rate} --gp-var {gp_var}' 
-                      f'--savedir "/cluster/home/amollers/Github/survival_analysis/run_pipeline/model_checkpoints"' 
+                      f'--num-mc {num_mc} --lr {learning_rate} --gp-var {gp_var} ' 
+                      f'--savedir "/cluster/home/amollers/Github/survival_analysis/run_pipeline/model_checkpoints" ' 
                       f'--arch "cpath_model_{learning_rate}_{gp_var}" '
-                      f'--log-dir {"/cluster/home/amollers/Github/survival_analysis/run_pipeline/model_checkpoints"} ')
+                      f'--log-dir "/cluster/home/amollers/Github/survival_analysis/run_pipeline/model_checkpoints"')
 
-                sub_command(cmd,f'--arch "cpath_model_{learning_rate}_{gp_var}"',hours=4)
+                sub_command(cmd,f'"cpath_model_{learning_rate}_{gp_var}"',hours=4)
